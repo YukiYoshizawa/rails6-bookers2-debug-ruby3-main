5309e5003e91b3f66afd71a5a8174b02d4b0a7e6
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-25 07:23:13 +0000

    [add] book/showでのいいね機能

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,20 +2,22 @@ class FavoritesController < ApplicationController
 
 def create
   book = Book.find(params[:book_id])
+   # indexの中から指定のbookを探してそれにいいねをつける必要がある。ルーティングでネストしているからbook_idも拾えている。
   favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
   # favorite = current_user.favorites.new(post_image_id: post_image.id)
   # どちらも同じ処理になる
 
   favorite.save
-  redirect_to books_path
-  # indexの中から指定のbookを探してそれにいいねをつける必要がある。ルーティングでネストしているからbook_idも拾えている。
+  redirect_to request.referer
+  # redirectに直前のURLを取得するメソッドを使って直前の画面に遷移する。
 end
+
 
 def destroy
   book = Book.find(params[:book_id])
   favorite = current_user.favorites.find_by(book_id: book.id)
   favorite.destroy
-  redirect_to books_path
+  redirect_to request.referer
 end
 
 end
