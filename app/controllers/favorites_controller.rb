70794ec4764f4c028eb9cee58bf2b7713fa06475
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-25 06:23:37 +0000

    [add]favorites/create

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+
+def create
+  book = Book.find(params[:book_id])
+  favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
+  # favorite = current_user.favorites.new(post_image_id: post_image.id)
+  # どちらも同じ処理になる
+
+  favorite.save
+  redirect_to books_path
+  # indexの中から指定のbookを探してそれにいいねをつける必要がある。ルーティングでネストしているからbook_idも拾えている。
+end
+
+def destroy
+end
+
+end
