e7c1d29bdaf094444c18e311db8bea4af51e5660
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-25 09:44:29 +0000

    [add] comment機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
     @show_user = User.find(@book_show.user_id)
     @book = Book.new
     @user = current_user
+    @book_comment = BookComment.new
   end
 
   def index
