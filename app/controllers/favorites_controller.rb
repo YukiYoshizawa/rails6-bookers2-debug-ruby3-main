a1e3623f4880fd98f1d6ff49720a4a6df21d871f
YukiYoshizawa <eatermgsorigin4@gmail.com>
2024-11-28 02:25:37 +0000

    [add]いいね機能の非同期化

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,23 +1,23 @@
 class FavoritesController < ApplicationController
 
 def create
-  book = Book.find(params[:book_id])
+  @book = Book.find(params[:book_id])
    # indexの中から指定のbookを探してそれにいいねをつける必要がある。ルーティングでネストしているからbook_idも拾えている。
-  favorite = Favorite.new(book_id: book.id, user_id: current_user.id)
+  favorite = Favorite.new(book_id: @book.id, user_id: current_user.id)
   # favorite = current_user.favorites.new(post_image_id: post_image.id)
   # どちらも同じ処理になる
 
   favorite.save
-  redirect_to request.referer
+  # redirect_to request.referer
   # redirectに直前のURLを取得するメソッドを使って直前の画面に遷移する。
 end
 
 
 def destroy
-  book = Book.find(params[:book_id])
-  favorite = current_user.favorites.find_by(book_id: book.id)
+  @book = Book.find(params[:book_id])
+  favorite = current_user.favorites.find_by(book_id: @book.id)
   favorite.destroy
-  redirect_to request.referer
+  # redirect_to request.referer
 end
 
 end
